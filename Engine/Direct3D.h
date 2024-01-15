1c78bb1a3c41fc415b1b2b25763610d9ff412ca1
武田一護 <220256@jc-21.jp>
2024-01-15 10:09:54 +0900

    HLSL追加したノーマルマップ

--- Engine/Direct3D.h
+++ Engine/Direct3D.h
@@ -18,6 +18,8 @@ enum SHADER_TYPE
 	SHADER_3D,
 	SHADER_POINT,
 	SHADER_TOON,
+	SHADER_OUTLINE,
+	SHADER_NORMALMAP,
 	SHADER_MAX,
 };
 
@@ -35,6 +37,8 @@ namespace Direct3D
 	HRESULT InitShader3D();
 	HRESULT InitShader2D();
 	HRESULT InitToonShader();
+	HRESULT InitToonOutline();
+	HRESULT InitNormalMap();
 	HRESULT InitShaderPointLight();
 
 
