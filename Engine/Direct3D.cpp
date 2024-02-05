16ca1d0c2d7759aace24cfb434484240db5689a0
武田一護 <220256@jc-21.jp>
2024-02-05 10:10:05 +0900

    エラーとったがTextureで例外

--- Engine/Direct3D.cpp
+++ Engine/Direct3D.cpp
@@ -160,17 +160,15 @@ HRESULT Direct3D::InitShader()
 	{
 		return E_FAIL;
 	}
-	//if (FAILED(InitShaderPointLight()))
-	//{
-	//	return E_FAIL;
-	//}
-	/*if (FAILED(InitToonShader()))
+	if (FAILED(InitShaderPointLight()))
 	{
 		return E_FAIL;
 	}
-	return S_OK;*/
-
-
+	if (FAILED(InitNormalMap()))
+	{
+		return E_FAIL;
+	}
+	return S_OK;
 }
 
 HRESULT Direct3D::InitShader3D()
@@ -244,78 +242,6 @@ HRESULT Direct3D::InitShader3D()
 	return S_OK;
 }
 
-//InitShader3Dをコピペして、名前をInitToonShade()に変更
-HRESULT Direct3D::InitToonShader()
-{
-	using namespace Direct3D;
-	HRESULT hr;
-	// 頂点シェーダの作成（コンパイル）
-	ID3DBlob* pCompileVS = nullptr;
-	D3DCompileFromFile(L"Toon.hlsl", nullptr, nullptr, "VS", "vs_5_0", NULL, 0, &pCompileVS, NULL);
-	assert(pCompileVS != nullptr); //ここはassertionで処理
-
-	hr = pDevice_->CreateVertexShader(pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(),
-		NULL, &(shaderBundle[SHADER_TOON].pVertexShader_));
-	if (FAILED(hr))
-	{
-		//エラー処理
-		MessageBox(NULL, "頂点シェーダーの作成に失敗しました", "エラー", MB_OK);
-		//解放処理
-		return hr;
-	}
-
-	//頂点インプットレイアウト
-	D3D11_INPUT_ELEMENT_DESC layout[] = {
-		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,  D3D11_INPUT_PER_VERTEX_DATA, 0 },	//位置
-		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, sizeof(DirectX::XMVECTOR) , D3D11_INPUT_PER_VERTEX_DATA, 0 },//UV座標
-		{ "NORMAL",	0, DXGI_FORMAT_R32G32B32_FLOAT, 0, sizeof(DirectX::XMVECTOR) * 2 ,	D3D11_INPUT_PER_VERTEX_DATA, 0 },//法線
-	};
-	hr = pDevice_->CreateInputLayout(layout, 3, pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(),
-		&(shaderBundle[SHADER_TOON].pVertexLayout_));
-	if (FAILED(hr))
-	{
-		//エラー処理
-		MessageBox(NULL, "頂点インプットレイアウトの作成に失敗しました", "エラー", MB_OK);
-		//解放処理
-		SAFE_RELEASE(pCompileVS);
-		return hr;
-	}
-	SAFE_RELEASE(pCompileVS);
-
-	// ピクセルシェーダの作成（コンパイル）
-	ID3DBlob* pCompilePS = nullptr;
-	D3DCompileFromFile(L"Toon.hlsl", nullptr, nullptr, "PS", "ps_5_0", NULL, 0, &pCompilePS, NULL);
-	assert(pCompilePS != nullptr);
-
-	hr = pDevice_->CreatePixelShader(pCompilePS->GetBufferPointer(), pCompilePS->GetBufferSize(), NULL, &(shaderBundle[SHADER_TOON].pPixelShader_));
-	if (FAILED(hr))
-	{
-		//エラー処理
-		MessageBox(NULL, "ピクセルシェーダの作成に失敗しました", "エラー", MB_OK);
-		SAFE_RELEASE(pCompilePS);
-		return hr;
-	}
-
-	SAFE_RELEASE(pCompilePS);
-
-	//ラスタライザ作成
-	D3D11_RASTERIZER_DESC rdc = {};
-	rdc.CullMode = D3D11_CULL_BACK;
-	rdc.FillMode = D3D11_FILL_SOLID;
-	rdc.FrontCounterClockwise = FALSE;
-	rdc.ScissorEnable = false;
-	rdc.MultisampleEnable = false;
-	hr = pDevice_->CreateRasterizerState(&rdc, &(shaderBundle[SHADER_TOON].pRasterizerState_));
-	if (FAILED(hr))
-	{
-		//エラー処理
-		MessageBox(NULL, "ラスタライザの作成に失敗しました", "エラー", MB_OK);
-		return hr;
-	}
-
-	return S_OK;
-}
-
 HRESULT Direct3D::InitShader2D()
 {
 	HRESULT hr;
@@ -339,7 +265,7 @@ HRESULT Direct3D::InitShader2D()
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,  D3D11_INPUT_PER_VERTEX_DATA, 0 },	//位置
 		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, sizeof(DirectX::XMVECTOR) , D3D11_INPUT_PER_VERTEX_DATA, 0 },//UV座標
 	};
-	hr = pDevice_->CreateInputLayout(layout.data(), layout.size(),
+	hr = pDevice_->CreateInputLayout(layout.data(), (UINT)layout.size(),
 		pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), &(shaderBundle[SHADER_2D].pVertexLayout_));
 	if (FAILED(hr))
 	{
@@ -406,14 +332,14 @@ HRESULT Direct3D::InitNormalMap()
 		return hr;
 	}
 
-	//頂点インプットレイアウト:sizeof(DirectX::XMVECTOR)
+	//頂点インプットレイアウト
 	D3D11_INPUT_ELEMENT_DESC layout[] = {
-		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,  0, D3D11_APPEND_ALIGNED_ELEMENT  , D3D11_INPUT_PER_VERTEX_DATA, 0  },	//位置
-		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,     0, D3D11_APPEND_ALIGNED_ELEMENT  , D3D11_INPUT_PER_VERTEX_DATA, 0 },    //UV座標
-		{ "NORMAL",	  0, DXGI_FORMAT_R32G32B32_FLOAT,  0, D3D11_APPEND_ALIGNED_ELEMENT  , D3D11_INPUT_PER_VERTEX_DATA, 0 },   //法線
-		{ "TAGENT",	  0, DXGI_FORMAT_R32G32B32_FLOAT,  0, D3D11_APPEND_ALIGNED_ELEMENT , D3D11_INPUT_PER_VERTEX_DATA, 0 },  //法線
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, sizeof(DirectX::XMVECTOR) * 0 , D3D11_INPUT_PER_VERTEX_DATA, 0 },	//位置
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,    0, sizeof(DirectX::XMVECTOR) * 1 , D3D11_INPUT_PER_VERTEX_DATA, 0 },	//UV
+		{ "NORMAL",	  0, DXGI_FORMAT_R32G32B32_FLOAT, 0, sizeof(DirectX::XMVECTOR) * 2 , D3D11_INPUT_PER_VERTEX_DATA, 0 },	//法線
+		{ "TANGENT",  0, DXGI_FORMAT_R32G32B32_FLOAT, 0, sizeof(DirectX::XMVECTOR) * 3 , D3D11_INPUT_PER_VERTEX_DATA, 0 },	//法線
 	};
-	hr = pDevice_->CreateInputLayout(layout, ARRAYSIZE(layout)/sizeof(D3D11_INPUT_ELEMENT_DESC), pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(),
+	hr = pDevice_->CreateInputLayout(layout, ARRAYSIZE(layout), pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(),
 		&(shaderBundle[SHADER_NORMALMAP].pVertexLayout_));
 	if (FAILED(hr))
 	{
@@ -430,7 +356,7 @@ HRESULT Direct3D::InitNormalMap()
 	D3DCompileFromFile(L"NormalMapping.hlsl", nullptr, nullptr, "PS", "ps_5_0", NULL, 0, &pCompilePS, NULL);
 	assert(pCompilePS != nullptr);
 
-	hr = pDevice_->CreatePixelShader(pCompilePS->GetBufferPointer(), pCompilePS->GetBufferSize(), NULL, &(shaderBundle[SHADER_3D].pPixelShader_));
+	hr = pDevice_->CreatePixelShader(pCompilePS->GetBufferPointer(), pCompilePS->GetBufferSize(), NULL, &(shaderBundle[SHADER_NORMALMAP].pPixelShader_));
 	if (FAILED(hr))
 	{
 		//エラー処理
@@ -448,7 +374,7 @@ HRESULT Direct3D::InitNormalMap()
 	rdc.FrontCounterClockwise = FALSE;
 	rdc.ScissorEnable = false;
 	rdc.MultisampleEnable = false;
-	hr = pDevice_->CreateRasterizerState(&rdc, &(shaderBundle[SHADER_3D].pRasterizerState_));
+	hr = pDevice_->CreateRasterizerState(&rdc, &(shaderBundle[SHADER_NORMALMAP].pRasterizerState_));
 	if (FAILED(hr))
 	{
 		//エラー処理
@@ -483,7 +409,6 @@ HRESULT Direct3D::InitShaderPointLight()
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,  D3D11_INPUT_PER_VERTEX_DATA, 0 },	//位置
 		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, sizeof(DirectX::XMVECTOR) , D3D11_INPUT_PER_VERTEX_DATA, 0 },//UV座標
 		{ "NORMAL",	0, DXGI_FORMAT_R32G32B32_FLOAT, 0, sizeof(DirectX::XMVECTOR) * 2 ,	D3D11_INPUT_PER_VERTEX_DATA, 0 },//法線
-		{ "TANGENT",	0, DXGI_FORMAT_R32G32B32_FLOAT, 0, sizeof(DirectX::XMVECTOR) * 3 ,	D3D11_INPUT_PER_VERTEX_DATA, 0 },//法線
 	};
 	hr = pDevice_->CreateInputLayout(layout, 3, pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(),
 		&(shaderBundle[SHADER_POINT].pVertexLayout_));
